b03ca12a8d6f7ba5d6491f6aa1cb5d85e7105a6e
ABDULLAHSHEAK <abdullahsheak8636@gmail.com>
2026-05-20 23:15:52 +0600

    mm

---
+++ miniproject/fevmovielist.py
@@ -0,0 +1,9 @@
+fevMoview = []
+while True:
+  name = input('Ener your fev Movie name: ')
+  fevMoview.append(name)
+  addnew = input('Add new moview yes/no ')
+  if(addnew == 'no'):
+    break
+
+print(fevMoview)
